c8b4bfa69f1979ed982e88c340af30f41ce4e839
hoangbach <petxuanbach@gmail.com>
2022-08-18 08:54:51 +0700

    test branch

--- app/src/main/java/peterbach1997/example/tasktimer/ChooseDaysSettingsFragment.kt
+++ app/src/main/java/peterbach1997/example/tasktimer/ChooseDaysSettingsFragment.kt
@@ -1,11 +1,22 @@
 package peterbach1997.example.tasktimer
 
 import android.os.Bundle
+import androidx.lifecycle.Lifecycle
 import androidx.preference.PreferenceFragmentCompat
 
 class ChooseDaysSettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+
+    override fun toString(): String {
+        return super<PreferenceFragmentCompat>.toString()
+        TODO("test")
+    }
+
+    override fun getLifecycle(): Lifecycle {
+        return super.getLifecycle()
+        TODO("TEST")
     }
 }
